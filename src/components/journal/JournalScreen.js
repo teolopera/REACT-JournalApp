af527e7956ee6d840bc5deaf00e3b5199db29ac6
mateo serna <mateoserna@MacBook-Pro-de-mateo.local>
2020-07-31 08:23:00 -0500

    SASS iniciando con el diseño de la aplicacion

---
+++ src/components/journal/JournalScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const JournalScreen = () => {
+    return (
+        <div>
+            <h1>Journal Screens</h1>
+        </div>
+    )
+}
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { AuthRouter } from './AuthRouter'
+import { JournalScreen } from '../components/journal/JournalScreen'
+
+export const AppRouter = () => {
+    return (
+
+        <Router>
+            
+            <Switch>
+
+                <Route 
+                    path="/auth"
+                    component={ AuthRouter }    
+                />
+                <Route
+                    exact
+                    path="/"
+                    component={ JournalScreen }
+                />
+
+                <Redirect to="/auth/login" />
+
+            </Switch>
+
+        </Router>
+
+    )
+}
