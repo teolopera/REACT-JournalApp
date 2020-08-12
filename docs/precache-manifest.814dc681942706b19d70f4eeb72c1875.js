672da35090500551ed1dd83d747d734742aacfa3
Mateo Serna <mateoslopera@gmail.com>
2020-08-12 12:35:40 -0500

    Github Pages Deploy

---
+++ docs/precache-manifest.814dc681942706b19d70f4eeb72c1875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04ac985aee0cf63c8f557dc8b9d3f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c96b98050bc2913ec61d",
+    "url": "/static/css/main.d2ae6e5c.chunk.css"
+  },
+  {
+    "revision": "b5d79544d079c76355ee",
+    "url": "/static/js/2.ed17912b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.ed17912b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96b98050bc2913ec61d",
+    "url": "/static/js/main.ca0dd9e1.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
